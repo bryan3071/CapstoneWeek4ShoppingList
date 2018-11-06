9a9db2c24013bc17d2063284e4720e308c691a24
bryan <bryan_hallman29@yahoo.com>
2018-11-05 20:41:19 -0500

    Capstone 4 Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+totalFood = 0;
+totalCost = 0;
+
+var AlmondChicken = document.getElementById("AlmondChicken");
+AlmondChicken.onclick = function() {
+	
+}
+
+var ChickenFriedRice = document.getElementById("addChickenFriedRice");
+var EggRolls = document.getElementById("addEggRolls");
+var SoftDrink = document.getElementById("addSoftDrink");

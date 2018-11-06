bb10ff655c629f862e45050de8f5cf658b5f1a2d
bryan <bryan_hallman29@yahoo.com>
2018-11-06 08:51:15 -0500

    Added much more working material

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-totalFood = 0;
-totalCost = 0;
+var totalCost = 0;
 
-var AlmondChicken = document.getElementById("AlmondChicken");
-AlmondChicken.onclick = function() {
+var cost= [];
+var foodChoices = [];
+//var order=  [];
+
+var add = document.getElementById("add");
+add.onclick = function() {
 	
+	foodChoices.push ("Almond Chicken ");
+	cost.push(3.0);
+	console.log(foodChoices + "$" + cost);
 }
 
-var ChickenFriedRice = document.getElementById("addChickenFriedRice");
-var EggRolls = document.getElementById("addEggRolls");
-var SoftDrink = document.getElementById("addSoftDrink");
+var add2 = document.getElementById("add2");
+add2.onclick = function() {
+	foodChoices.push ("Chicken Fried Rice ");
+	cost.push(5.50);
+	console.log(foodChoices + "$" + cost);
+}
+
+var add3 = document.getElementById("add3");
+add3.onclick = function() {
+	foodChoices.push ("Egg Rolls ");
+	cost.push(1.50);
+	console.log(foodChoices + "$" + cost);
+	}
+
+
+var add4 = document.getElementById("add4");
+add4.onclick = function() {
+	foodChoices.push ("Soft Drink ");
+	cost.push(2.00);
+	console.log(foodChoices + "$" + cost);
+	}
+
+var add5 = document.getElementById("add5");
+add5.onclick = function() {
+for (var i=0; i<cost.length; i++);{
+	console.log (foodChoices[i] + cost[i]);
+	totalCost += cost[i];
+} console.log(totalCost);
+//console.log(cost[i]);
+}
